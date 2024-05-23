82e8f4ed812a0d4946b26c23deb9ee6ec6a6bf4f
NathanFallet <contact@nathanfallet.me>
2024-05-23 15:26:16 +0200

    oups

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("multiplatform").version("2.0.0").apply(false)
     kotlin("plugin.serialization").version("2.0.0").apply(false)
     id("convention.publication").apply(false)
-    id("org.jetbrains.kotlinx.kover").version("0.7.4").apply(false)
+    id("org.jetbrains.kotlinx.kover").version("0.8.0").apply(false)
     id("com.google.devtools.ksp").version("2.0.0-1.0.21").apply(false)
 }
 
